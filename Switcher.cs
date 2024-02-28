a09938a228c821efacd9efc296a423654ad7e389
Adam Samec <adam.samec@gmail.com>
2024-02-28 17:15:41 +0100

    Remove NonInvasiveKeyboadHookManager library and add app switching

--- Switcher.cs
+++ Switcher.cs
@@ -5,8 +5,6 @@ using System.Media;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-using NonInvasiveKeyboardHookLibrary;
-
 namespace WinSwitcher
 {
     /// <summary>
@@ -16,14 +14,12 @@ namespace WinSwitcher
     {
         private Process _currentProcess = Process.GetCurrentProcess();
         private MainWindow _mainWindow;
-        private static KeyboardHookManager _hook;
+        private KeyboardHook _hook;
         private List<Process> _processesList = new List<Process>();
 
-        private const NonInvasiveKeyboardHookLibrary.ModifierKeys _appsShortcutModifier = NonInvasiveKeyboardHookLibrary.ModifierKeys.WindowsKey | NonInvasiveKeyboardHookLibrary.ModifierKeys.Shift;
-        private const int _appsShortcutKey = 0x41; // Letter "A"
+        //private const NonInvasiveKeyboardHookLibrary.ModifierKeys _appsShortcutModifier = NonInvasiveKeyboardHookLibrary.ModifierKeys.WindowsKey | NonInvasiveKeyboardHookLibrary.ModifierKeys.Shift;
+        //private const int _appsShortcutKey = 0x41; // Letter "A"
         
-        private static KeyboardHookManager Hook => _hook ?? (_hook = new KeyboardHookManager());
-
         [DllImport("User32.dll")]
         private static extern bool SetForegroundWindow(IntPtr handle);
 
@@ -39,12 +35,9 @@ namespace WinSwitcher
 
         private void InitKeyboardHook()
         {
-            Hook.Start();
-            Hook.RegisterHotkey(_appsShortcutModifier, _appsShortcutKey, () => {
-                Application.Current.Dispatcher.Invoke(delegate
-                {
+            _hook = new KeyboardHook(_mainWindow, VirtualKeyCodes.A, ModifierKeyCodes.Windows | ModifierKeyCodes.Shift);
+            _hook.Triggered += () => {  
                     SystemSounds.Beep.Play();
-                    Hook.Stop();
 
                     Process[] processes = Process.GetProcesses();
                     List<string> appTitlesList = new List<string>();
@@ -61,9 +54,7 @@ namespace WinSwitcher
 
                     _mainWindow.SetItems(appTitlesList);
                     _mainWindow.Display();
-                Hook.Start();
-                });
-            });
+            };
         }
 
         public void SwitchToItem(int itemNum)
@@ -76,8 +67,7 @@ namespace WinSwitcher
 
         public void CleanUp()
         {
-            Hook.UnregisterAll();
-            Hook.Stop();
+            _hook.Dispose();
         }
     }
 }
