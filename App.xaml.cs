ff13f007648a792d376e2ee631bb914f1067ffd1
Adam Samec <adam.samec@gmail.com>
2024-03-07 18:37:27 +0100

    Add base for globalization

--- App.xaml.cs
+++ App.xaml.cs
@@ -9,6 +9,27 @@ namespace WinSwitcher
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SetLanguageDictionary();
+        }
+
+        private void SetLanguageDictionary()
+        {
+            switch (Thread.CurrentThread.CurrentCulture.ToString())
+            {
+                case "cs-CZ":
+                    WinSwitcher.Resources.Culture = new System.Globalization.CultureInfo("cs-CZ");
+                    break;
+                default:
+                    WinSwitcher.Resources.Culture = new System.Globalization.CultureInfo("en-US");
+                    break;
+            }
+
+
+        }
+
     }
 
 }
